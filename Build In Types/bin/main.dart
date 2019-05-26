d50a9a60f92efca4eb708d3f246f1832e9118ced
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-05-26 22:20:51 +0530

    learn bulid in types num

---
+++ Build In Types/bin/main.dart
@@ -0,0 +1,16 @@
+main(List<String> arguments) {
+  var num1=45;
+  var num2=78;
+  var num3=89.65;
+  var num4=15.394;
+  var st1='abcdefgh';
+
+  //return the absolute value
+  print(num3.abs());
+
+  //return the least integer no smaller than
+  print(num4.ceil());
+
+  //return the least double value
+  print(num3.ceilToDouble());
+}
