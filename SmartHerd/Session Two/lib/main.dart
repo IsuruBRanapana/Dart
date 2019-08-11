a47baabb7abaf5acf71296b6fa01657a3ae67656
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-11 17:49:33 +0530

    updated

--- SmartHerd/Session Two/lib/main.dart
+++ SmartHerd/Session Two/lib/main.dart
@@ -2,8 +2,16 @@ import 'numbers.dart';
 import 'words.dart';
 
  void main() {
+   print('Numbers');
+   print("");
+   print("");
    numbers1();
    numbers2();
 
+   print(' ');
+   print(' ');
+   print('String');
+   print("");
+   print("");
    w1();
  }
--- SmartHerd/Session Two/lib/words.dart
+++ SmartHerd/Session Two/lib/words.dart
@@ -1,6 +1,8 @@
 void w1(){
   var s1='Hello dart. hello everyone';
   var s2="looks";
+  var s3='  look\'s like   ';
+  var s4="abcd 15 efgh 45";
 
   //split()
   print(s1.split(" "));
@@ -14,5 +16,22 @@ void w1(){
   //endsWith()
   print(s2.endsWith('f'));
 
-  
+  //subString()
+  print(s1.substring(6,10));  
+
+  //toUpperCase()
+  print(s1.toUpperCase());
+
+  //toLowerCase()
+  print(s2.toLowerCase());
+
+  //trim()
+  print(s3.trim());
+
+  //replaceAll()
+  print(s1.replaceAll('hello', 'bye'));
+
+  //regular expression
+  var num1=RegExp(r'\d+');
+  print(s4.replaceAll(num1, 'xx'));
 }
