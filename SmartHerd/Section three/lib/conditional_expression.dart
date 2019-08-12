83c6ffe3057922a80c88da7b1f50741057dd8315
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-12 19:35:28 +0530

    Conditional Expression

---
+++ SmartHerd/Section three/lib/conditional_expression.dart
@@ -0,0 +1,31 @@
+void f2(){
+  //if statement
+  int a=45;
+  int b=55;
+
+  if (a<b){
+    print('$a is smaller');
+  }else{
+    print('$b is smaller');
+  }
+
+  // above one using conditional expression
+  a<b ? print('$a is smaller') : print('$b is smaller');
+
+
+  //if statement
+  int x=78;
+  int y=85;
+  int smaller;
+  if(x<y){
+    smaller=x;
+  }else{
+    smaller=y;
+  }
+  print('$smaller is smaller one');
+
+
+  //above one using conditional expression
+  smaller=x<y ? x : y;
+  print('$smaller is smaller one');
+}
--- SmartHerd/Section three/lib/main.dart
+++ SmartHerd/Section three/lib/main.dart
@@ -1,8 +1,16 @@
 import 'if_else.dart';
+import 'conditional_expression.dart';
 
 void main(){
   print('If - Else statement');
   print('');
   print('');
   f1();
+
+  print('');
+  print('');
+  print('Conditional Expression');
+  print('');
+  print('');
+  f2();
 }
