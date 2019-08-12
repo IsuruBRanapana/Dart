ee8c81c97dcd7e7a90351a9e2f9ce0df8febe182
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-12 19:00:43 +0530

    Updated

--- SmartHerd/Section three/lib/if_else.dart
+++ SmartHerd/Section three/lib/if_else.dart
@@ -1,8 +1,6 @@
 void f1(){
   var x=75;
-  if(x>100){
-    print('Wrong marks');
-  }else if(x<=100 && x>75){
+  if(x<=100 && x>75){
     print('You have A grade');
   }else if(x<=75 && x>65){
     print('You have B grade');
@@ -10,7 +8,9 @@ void f1(){
     print('You have C grade');
   }else if(x<=55 && x>35){
     print('You have S grade');
-  }else{
+  }else if(x<=35 && x>0){
     print('You are Fail');
+  }else{
+    print('Invalid marks');  
   }
 }
