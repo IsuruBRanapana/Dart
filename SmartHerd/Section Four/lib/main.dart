12cd33a28400dc337b30e72f6f4b6aa5230394ca
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-13 19:55:14 +0530

    While loop

--- SmartHerd/Section Four/lib/main.dart
+++ SmartHerd/Section Four/lib/main.dart
@@ -1,4 +1,5 @@
 import 'for.dart';
+import 'while.dart';
 void main() {
   print('');
   print('');
@@ -6,4 +7,11 @@ void main() {
   print('');
   print('');
   f1();
+
+  print('');
+  print('');
+  print('WHILE loop');
+  print('');
+  print('');
+  f2();
 }
---
+++ SmartHerd/Section Four/lib/while.dart
@@ -0,0 +1,17 @@
+void f2(){
+  //print 1 to 10
+  var x=1;
+  while(x<=10){
+    print(x);
+    x++;
+  }
+
+  //odd numbers 1to 10
+  print('');
+  print('');
+  var y=1;
+  while(y<=10){
+    y%2!=0?print(y):null;
+    y++;
+  }
+}
