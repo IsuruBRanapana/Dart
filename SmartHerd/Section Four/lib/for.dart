034f93b3409c1718538f39ea6862897ac7f64dd7
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-13 19:40:04 +0530

    For loop

--- SmartHerd/Section Four/lib/for.dart
+++ SmartHerd/Section Four/lib/for.dart
@@ -1,3 +1,13 @@
 void f1(){
-  
+  //print 1 to 20
+  for(var i = 1;i<=20;i++){
+    print(i);
+  }
+
+  //even numbers one to 10
+  print('');
+  print('');
+  for(var j=1;j<=10;j++){
+    j%2==0?print(j):null;
+  }
 }
