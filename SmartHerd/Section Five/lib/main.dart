63f514af231bae65f7a8f9c244ff92cef27ae174
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-14 11:31:53 +0530

    Updated

--- SmartHerd/Section Five/lib/main.dart
+++ SmartHerd/Section Five/lib/main.dart
@@ -2,6 +2,8 @@ void main() {
   //must call the functions
   findArea();
   findSum(85, 145);
+  int fMul=findMul(7, 12);
+  print(fMul);
 }
 
 
@@ -17,4 +19,10 @@ void findArea(){
 void findSum(int x, int y){
   int z=x+y;
   print('The sum of $x and $y is $z');
+}
+
+//return value
+int findMul(int a, int b){
+  int c=a*b;
+  return c;
 }
