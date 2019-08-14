0d63adcd2f41ec9ab2fbbd378bbff442e7fe6e69
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-14 11:20:04 +0530

    Start to learn functions

---
+++ SmartHerd/Section Five/lib/main.dart
@@ -0,0 +1,13 @@
+void main() {
+  //must call the functions
+  findArea();
+}
+
+
+// without parameters
+void findArea(){
+  int a=89;
+  int b=5;
+  int area = a*b;
+  print('The area is $area');
+}
