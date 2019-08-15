6760910b8ed7c23b6813f022370cef68c9c94e34
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-15 16:33:55 +0530

    Updated

--- SmartHerd/Section Six/lib/main.dart
+++ SmartHerd/Section Six/lib/main.dart
@@ -16,4 +16,14 @@ void main() {
   }catch(e){
     print('Exception is $e');
   }
+
+  print('Case Three');
+  //dont know the exception. Use StackTrace
+  try{
+    int result = 12~/0;
+    print('Result is $result');
+  }catch(e,s){
+    print('Exception is $e');
+    print('StackTrace is \n $s');
+  }
 }
