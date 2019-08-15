0f52d2116e1a78fd526442770194a78a4225b5ac
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-15 16:26:02 +0530

    Updated

--- SmartHerd/Section Six/lib/main.dart
+++ SmartHerd/Section Six/lib/main.dart
@@ -7,4 +7,13 @@ void main() {
   }on IntegerDivisionByZeroException{
     print('Cannot divided by zero');
   }
+
+  print('Case Two');
+  //dont know the exception
+  try{
+    int result = 12~/0;
+    print('Result is $result');
+  }catch(e){
+    print('Exception is $e');
+  }
 }
