bf1101436afd16f4909f0549d3f280f5f96298e2
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-16 16:26:20 +0530

    Start to learn Classes in dart

---
+++ SmartHerd/Section Seven/lib/main.dart
@@ -0,0 +1,23 @@
+ void main() {
+   
+ }
+
+ //define a class
+ //add properties and behaviours
+
+ class Student{
+   //properties
+   int id;
+   String name;
+   String homeAddress;
+   
+   
+   //behaviours
+   void read(){
+     print('Student can read');
+   }
+
+   void sleep(){
+     print('Student can sleep');
+   }
+ }
