a5d3280ccb893a5f5cb07194d4986de22e7fbaee
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-16 16:38:44 +0530

    Updated

--- SmartHerd/Section Seven/lib/main.dart
+++ SmartHerd/Section Seven/lib/main.dart
@@ -1,5 +1,17 @@
  void main() {
-   
+   //make objects
+   var st1= new Student();
+   st1.id = 45;
+   st1.name = 'Peter';
+   print('Id is ${st1.id} and Name is ${st1.name}');
+   st1.read();
+
+   //another object   
+   var st2 =Student();
+   st2.id = 55;
+   st2.name='Mary';
+   print('Id is ${st2.id} and Name is ${st2.name}');
+   st2.sleep();
  }
 
  //define a class
@@ -14,10 +26,10 @@
    
    //behaviours
    void read(){
-     print('Student can read');
+     print('${this.name} is now reading');
    }
 
    void sleep(){
-     print('Student can sleep');
+     print('${this.name} in now sleeping');
    }
  }
