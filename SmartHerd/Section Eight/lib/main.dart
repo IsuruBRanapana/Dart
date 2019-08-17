6ff37c9aa9d7deec0453d7125b910ad93980ca48
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-17 22:39:26 +0530

    Update main.dart
    Learn custom setter and getter

--- SmartHerd/Section Eight/lib/main.dart
+++ SmartHerd/Section Eight/lib/main.dart
@@ -2,8 +2,21 @@ void main() {
   var st1=Student();
   st1.name="Saman"; //default setter to set value
   print(st1.name); //default getter to get value
+
+  st1.percentage=479;//custom setter to set value
+  print(st1.percentage);//custom getter to get value
 }
 
 class Student{
   String name;
+  double percent;
+  //custom setter
+  void set percentage(double marksSecure){
+    percent = (marksSecure/500)*100;
+  }
+
+  //custom getter
+  double get percentage{
+    return percent;
+  }
 }
