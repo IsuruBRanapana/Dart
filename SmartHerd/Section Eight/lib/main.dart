084e702b439e0ada825a97dc875e92b60e2b16bf
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-18 11:29:20 +0530

    Update main.dart
    private instance variable

--- SmartHerd/Section Eight/lib/main.dart
+++ SmartHerd/Section Eight/lib/main.dart
@@ -5,11 +5,15 @@ void main() {
 
   st1.percentage=479;//custom setter to set value
   print(st1.percentage);//custom getter to get value
+
+  st1.average=78;
+  print(st1.average);
 }
 
 class Student{
   String name;
   double percent;
+  double _ave;
   //custom setter
   void set percentage(double marksSecure){
     percent = (marksSecure/500)*100;
@@ -19,4 +23,7 @@ class Student{
   double get percentage{
     return percent;
   }
+
+  void set average(double a) => _ave=(a+45)/2;
+  double get average => _ave;
 }
