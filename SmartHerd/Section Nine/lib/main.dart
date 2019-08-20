f565d409336cb3c3bcfe6152ed7fc53c2fa99ff2
IsuruBRanapana <31366873+IsuruBRanapana@users.noreply.github.com>
2019-08-20 20:23:41 +0530

    Update main.dart
    second way

--- SmartHerd/Section Nine/lib/main.dart
+++ SmartHerd/Section Nine/lib/main.dart
@@ -15,6 +15,12 @@ void main(){
   print(number(8));
 
   //second way flat arrow method
+  Function twoNumbers1 =(int a, int b) =>print(a+b);
+  var number1 = (int num1) => num1*7;
+
+  //calling
+  twoNumbers1(75,85);
+  print(number1(8));
 }
 
 //normal function
